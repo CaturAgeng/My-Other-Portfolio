a0aad7da1f5ab535ca99670ebe907b3f55569f23
CaturAgeng <caturageng023@gmail.com>
2024-08-18 14:26:04 +0700

    Add header

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import profile1 from '@/public/images/profile1.jpg';
 
 const Header = () => {
   return (
-    <header className="w-full p-4 sm:p-8 text-center bg-black">
+    <header className="max-w-[1108px] mx-auto text-gray-400 p-4 sm:p-8 md:p-16 items-center justify-center flex flex-col bg-black">
       <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-6xl">
         Hello,{' '}
         <ReactTypingEffect
@@ -18,14 +18,15 @@ const Header = () => {
         />
       </h1>
       <p className="my-16 sm:mt-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-        Software Engineer, Frontend/Backend.
+        Software Engineer.
       </p>
       <Image 
         src={profile1} 
         alt="Catur Ageng" 
-        width={992} 
-        height={1108} 
-        className='mx-auto block grayscale w-3/4 sm:w-3/4 md:w-3/4 lg:w-3/4 xl:w-1/2'
+        width={2000} 
+        height={2000} 
+        objectFit='cover'
+        className='w-full grayscale filter transition duration-300 ease-in-out hover:filter-none'
       />
     </header>
   );
